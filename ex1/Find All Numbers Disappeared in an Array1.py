5b0110e85402accc25c230bbb6459201bf837612
Sugitani Miwa <miwa.sugitani@hogeticlab.com>
2024-02-28 21:37:27 +0900

    ex1

--- ex1/Find All Numbers Disappeared in an Array1.py
+++ ex1/Find All Numbers Disappeared in an Array1.py
@@ -1,13 +1,14 @@
 class Solution(object):
     def findDisappearedNumbers(self, nums):
         n = len(nums)
-        l = []
 
-        for i in range(1,n+1):
+        for i in range(1, n + 1):
+            judge = 0
             for j in range(n):
                 if nums[j] == i:
+                    judge = 1
                     break
-            else:
-                l.append(i)
+            if judge == 0:
+                nums.append(i)
 
-        return l
+        return nums[n:]
